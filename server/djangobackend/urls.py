b2b767c237defcb00c2d1c52f46bec56922e6f0a
tobilobade <dadetugboboh1@gmail.com>
2023-07-18 10:08:33 -0400

    Adding temporary changes to Github

--- server/djangobackend/urls.py
+++ server/djangobackend/urls.py
@@ -21,4 +21,5 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('djangoapp/', include('djangoapp.urls')),
+    path('djangoapp/about', include('djangoapp.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
