78f5fe54300facfe284a10e49b498a19a778971a
Adetugboboh Damilola <dadetugboboh1@gmail.com>
2023-07-20 12:50:04 +0100

    Update models.py

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,6 +1,24 @@
 from django.db import models
 from django.utils.timezone import now
 
+class CarMake(models.Model):
+    name = models.CharField(null=False, max_length=30)
+    description = models.CharField(null=False, max_length=300)
+
+    def __str__(self):
+        return "Name:" + self.name + "\nDescription:" + self.description
+
+
+class CarModel(models.Model):
+    make = models.ForeignKey(CarMake, on_delete=models.CASCADE)
+    name = models.CharField(null=False, max_length=30)
+    dealerId = models.IntegerField()
+    choices = ('SEDAN', "Sedan"), ('SUV', "SUV"), ('WAGON', "Wagon"), ('TRUCK', "Truck")
+    carType = models.CharField(null=False, choices=choices, max_length=20)
+    year = models.DateField()
+
+    def __str__(self):
+        return "Name:" + self.name + "\Make:" + str(self.make)
 
 # Create your models here.
 
